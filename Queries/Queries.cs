c07ae9f1d06e1dc5f4b760422aca6fad5fe60aff
John Strand <john.strand@dekiru.se>
2020-01-21 17:09:03 +0100

    Added some code generation for embedded queries

---
+++ Queries/Queries.cs
@@ -0,0 +1,8 @@
+﻿namespace ScaffoldEF
+{
+	public static class Query
+	{
+		public static string ExportForeignKeys { get; } = typeof(Program).Assembly.GetManifestResourceStream("ScaffoldEF.Queries.ExportForeignKeys.sql").ReadAndClose();
+		public static string ExportTables { get; } = typeof(Program).Assembly.GetManifestResourceStream("ScaffoldEF.Queries.ExportTables.sql").ReadAndClose();
+	}
+}
