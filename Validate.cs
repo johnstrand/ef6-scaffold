0ceed2851cf6a537870f71b0746826728f20da8a
John Strand <john.strand@dekiru.se>
2020-01-21 16:00:12 +0100

    Added validation extension

---
+++ Validate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ScaffoldEF
+{
+    internal static class Validate
+    {
+        internal static bool AssertIsTrue(this bool value, params string[] error)
+        {
+            if (!value)
+            {
+                throw new Exception(string.Join(Environment.NewLine, error));
+            }
+            return value;
+        }
+
+        internal static bool AssertIsFalse(this bool value, params string[] error)
+        {
+            if (value)
+            {
+                throw new Exception(string.Join(Environment.NewLine, error));
+            }
+            return value;
+        }
+    }
+}
