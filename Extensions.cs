d776de3e0ff48a445e0fd01df150140bbb9013b5
John Strand <john.strand@dekiru.se>
2020-01-21 17:08:32 +0100

    Renamed extensions file

--- Extensions.cs
+++ Extensions.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.IO;
 
 namespace ScaffoldEF
 {
-    internal static class Validate
+    internal static class Extensions
     {
         internal static bool AssertIsTrue(this bool value, params string[] error)
         {
@@ -20,6 +21,12 @@ namespace ScaffoldEF
                 throw new Exception(string.Join(Environment.NewLine, error));
             }
             return value;
+        }
+
+        internal static string ReadAndClose(this Stream stream)
+        {
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
         }
     }
 }
