6055990e8202e9877883eb5afafb578504db5ebd
John Strand <john.strand@dekiru.se>
2020-01-21 15:59:32 +0100

    Added a database client

---
+++ Data/DbClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ScaffoldEF.Data
+{
+    internal class DbClient : IDisposable
+    {
+        private readonly SqlConnection connection;
+        public DbClient(string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
+        }
+    }
+}
