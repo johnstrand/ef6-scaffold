56bf6c3959e85b3fcf950b69dccd1a43a6e5bd47
John Strand <john.strand@dekiru.se>
2020-01-21 17:10:46 +0100

    Added WIP export command for foreign keys

---
+++ Commands/Export.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScaffoldEF.Commands
+{
+    [Command("export")]
+    static class Export
+    {
+        [Command("fk")]
+        internal static void ForeignKeys(string target)
+        {
+            Settings.ConnectionManager.TryGet(target, out var connection).AssertIsTrue($"Connection '{target}' is not configured");
+            using var db = new Data.DbClient(connection);
+            var keys = db.Query<Data.ForeignKeysWrapper>(Query.ExportForeignKeys).ForeignKeys;
+        }
+    }
+}
