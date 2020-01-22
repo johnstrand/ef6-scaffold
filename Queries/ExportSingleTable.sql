30d8a22e4976a68aa430d4889878456aa90bbd24
John Strand <john.strand@dekiru.se>
2020-01-22 12:01:48 +0100

    Added support for exporting a single table

---
+++ Queries/ExportSingleTable.sql
@@ -0,0 +1,27 @@
+﻿WITH pk AS (SELECT ixc.object_id, ixc.column_id FROM sys.indexes ix
+JOIN sys.index_columns ixc ON ix.object_id = ixc.object_id AND ix.index_id = ixc.index_id
+WHERE ix.is_primary_key = 1)
+
+SELECT
+	s.name [@schema],
+	t.name [@name],
+	(
+		SELECT
+		c.name,
+		c.is_identity,
+		c.is_nullable,
+		c.is_rowguidcol,
+		c.max_length,
+		c.precision,
+		c.scale,
+		tt.name [type],
+		CASE WHEN EXISTS (SELECT NULL FROM pk p WHERE c.object_id = p.object_id AND c.column_id = p.column_id) THEN 1 ELSE 0 END [is_primary_key]
+		FROM sys.columns c
+		JOIN sys.types tt ON c.user_type_id = tt.user_type_id
+		WHERE t.object_id = c.object_id
+		FOR XML PATH('column'), TYPE
+	)
+	FROM sys.schemas s
+	JOIN sys.tables t ON s.schema_id = t.schema_id
+	WHERE s.name = @schema AND t.name = @table
+	FOR XML PATH('table')
--- Queries/Queries.cs
+++ Queries/Queries.cs
@@ -3,6 +3,7 @@
 	public static class Query
 	{
 		public static string ExportForeignKeys { get; } = typeof(Program).Assembly.GetManifestResourceStream("ScaffoldEF.Queries.ExportForeignKeys.sql").ReadAndClose();
+		public static string ExportSingleTable { get; } = typeof(Program).Assembly.GetManifestResourceStream("ScaffoldEF.Queries.ExportSingleTable.sql").ReadAndClose();
 		public static string ExportTables { get; } = typeof(Program).Assembly.GetManifestResourceStream("ScaffoldEF.Queries.ExportTables.sql").ReadAndClose();
 	}
 }
