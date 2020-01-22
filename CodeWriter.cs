09550dea8034b6414c344847a39ee618f95c7789
John Strand <john.strand@dekiru.se>
2020-01-22 12:11:45 +0100

    Moved code generation into CodeWriter

--- CodeWriter.cs
+++ CodeWriter.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ScaffoldEF.Data;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ScaffoldEF
 {
@@ -11,6 +14,93 @@ namespace ScaffoldEF
         {
             writer = new StreamWriter(filename);
         }
+
+        public void WriteTable(Table table) => WriteTable(table, new List<ForeignKey>());
+
+        public void WriteTable(Table table, List<ForeignKey> foreignKeys)
+        {
+            WriteText($"[Table(\"{table.Schema}.{table.Name}\")]");
+            BeginBlock($"public class {table.Name}");
+
+            foreach (var column in table.Columns)
+            {
+                if (column.IsPrimaryKey)
+                {
+                    WriteText("[Key]");
+                    if (!column.IsIdentity)
+                    {
+                        WriteText("[DatabaseGenerated(DatabaseGeneratedOption.None)]");
+                    }
+                }
+
+                if (column.Type.Contains("char"))
+                {
+                    if (!column.IsNullable)
+                    {
+                        WriteText("[Required]");
+                    }
+
+                    WriteText($"[StringLength({(column.MaxLength > 0 ? column.MaxLength.ToString() : "MAX")})]");
+                }
+
+                if (TypeMap.TryGet(column.Type, out var type))
+                {
+                    column.Type = type;
+                }
+                else
+                {
+                    throw new Exception($"Found to type map matching {column.Type}");
+                }
+
+                if (column.IsNullable && column.Type != "string")
+                {
+                    column.Type += "?";
+                }
+
+                WriteText($"public {column.Type} {column.Name} {{ get; set; }}");
+            }
+
+            foreach (var fk in foreignKeys.Where(fk => fk.FromSchema == table.Schema && fk.FromTable == table.Name))
+            {
+                WriteText($"public virtual {fk.ToSchema}.{fk.ToTable} {FormatFKName(fk, FKDirection.From)} {{ get; set; }}");
+                WriteLine();
+            }
+
+            foreach (var fk in foreignKeys.Where(fk => fk.ToSchema == table.Schema && fk.ToTable == table.Name))
+            {
+                WriteText($"public virtual ICollection<{fk.FromSchema}.{fk.FromTable}> {FormatFKName(fk, FKDirection.To)} {{ get; set; }}");
+                WriteLine();
+            }
+
+            var relations = foreignKeys.Where(fk => fk.ToSchema == table.Schema && fk.ToTable == table.Name).ToList();
+
+            if (relations.Any())
+            {
+                BeginBlock("public static void Configure(DbModelBuilder modelBuilder");
+                foreach (var relation in relations)
+                {
+                    WriteText($"modelBuilder.Entity<{table.Name}>()");
+                    Indent();
+                    WriteText($".HasMany(entity => entity.{FormatFKName(relation, FKDirection.To)})");
+                    // TODO: WithOptional
+                    WriteText($".WithRequired(entity => entity.{FormatFKName(relation, FKDirection.From)})");
+                    if (relation.Columns.Count > 1)
+                    {
+                        WriteText($".HasForeignKey(entity => new {{ {string.Join(", ", relation.Columns.Select(c => $"entity.{c.From}"))} }});");
+                    }
+                    else
+                    {
+                        WriteText($".HasForeignKey(entity => entity.{relation.Columns.First().From});");
+                    }
+                    Deindent();
+                }
+                EndBlock();
+            }
+
+            EndBlock();
+            EndBlock();
+        }
+
         public void WriteText(string text)
         {
             WriteIndent();
@@ -56,6 +146,30 @@ namespace ScaffoldEF
         public void Dispose()
         {
             writer.Dispose();
+        }
+
+        // TODO: Make this less insane
+        private enum FKDirection
+        {
+            To,
+            From
+        }
+
+        private static string FormatFKName(ForeignKey fk, FKDirection direction)
+        {
+            var name = (fk.Alias ?? fk.Name);
+            if (name.StartsWith("FK_"))
+            {
+                name = name.Substring(3);
+            }
+
+            if (name.Contains("!"))
+            {
+                var parts = name.Split('!');
+                return direction == FKDirection.To ? parts[0] : parts[1];
+            }
+
+            return name;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,97 +12,6 @@ namespace ScaffoldEF
             try
             {
                 Exec.Run(args);
-                /*
-                var definition = Definition.Load(File.OpenRead("export.xml"));
-                using var output = new CodeWriter("export.txt");
-                foreach (var schema in definition.Schemas)
-                {
-                    foreach (var table in schema.Tables)
-                    {
-                        output.BeginBlock($"namespace DataLayer.{schema.Name}");
-                        output.WriteText($"[Table(\"{schema.Name}.{table.Name}\")]");
-                        output.BeginBlock($"public class {table.Name}");
-
-                        foreach (var column in table.Columns)
-                        {
-                            if (column.IsPrimaryKey)
-                            {
-                                output.WriteText("[Key]");
-                                if (!column.IsIdentity)
-                                {
-                                    output.WriteText("[DatabaseGenerated(DatabaseGeneratedOption.None)]");
-                                }
-                            }
-
-                            if (column.Type.Contains("char"))
-                            {
-                                if (!column.IsNullable)
-                                {
-                                    output.WriteText("[Required]");
-                                }
-
-                                output.WriteText($"[StringLength({(column.MaxLength > 0 ? column.MaxLength.ToString() : "MAX")})]");
-                            }
-
-                            if (TypeMap.TryGet(column.Type, out var type))
-                            {
-                                column.Type = type;
-                            }
-                            else
-                            {
-                                throw new Exception($"Found to type map matching {column.Type}");
-                            }
-
-                            if (column.IsNullable && column.Type != "string")
-                            {
-                                column.Type += "?";
-                            }
-
-                            output.WriteText($"public {column.Type} {column.Name} {{ get; set; }}");
-                        }
-
-                        foreach (var fk in definition.ForeignKeys.Where(fk => fk.FromSchema == schema.Name && fk.FromTable == table.Name))
-                        {
-                            output.WriteText($"public virtual {fk.ToSchema}.{fk.ToTable} {FormatFKName(fk, FKDirection.From)} {{ get; set; }}");
-                            output.WriteLine();
-                        }
-
-                        foreach (var fk in definition.ForeignKeys.Where(fk => fk.ToSchema == schema.Name && fk.ToTable == table.Name))
-                        {
-                            output.WriteText($"public virtual ICollection<{fk.FromSchema}.{fk.FromTable}> {FormatFKName(fk, FKDirection.To)} {{ get; set; }}");
-                            output.WriteLine();
-                        }
-
-                        var relations = definition.ForeignKeys.Where(fk => fk.ToSchema == schema.Name && fk.ToTable == table.Name).ToList();
-
-                        if (relations.Any())
-                        {
-                            output.BeginBlock("public static void Configure(DbModelBuilder modelBuilder");
-                            foreach (var relation in relations)
-                            {
-                                output.WriteText($"modelBuilder.Entity<{table.Name}>()");
-                                output.Indent();
-                                output.WriteText($".HasMany(entity => entity.{FormatFKName(relation, FKDirection.To)})");
-                                // TODO: WithOptional
-                                output.WriteText($".WithRequired(entity => entity.{FormatFKName(relation, FKDirection.From)})");
-                                if (relation.Columns.Count > 1)
-                                {
-                                    output.WriteText($".HasForeignKey(entity => new {{ {string.Join(", ", relation.Columns.Select(c => $"entity.{c.From}"))} }});");
-                                }
-                                else
-                                {
-                                    output.WriteText($".HasForeignKey(entity => entity.{relation.Columns.First().From});");
-                                }
-                                output.Deindent();
-                            }
-                            output.EndBlock();
-                        }
-
-                        output.EndBlock();
-                        output.EndBlock();
-                    }
-                }
-                */
             }
             catch (TargetInvocationException tex)
             {
@@ -114,28 +23,6 @@ namespace ScaffoldEF
             }
         }
 
-        private enum FKDirection
-        {
-            To,
-            From
-        }
-
-        private static string FormatFKName(ForeignKey fk, FKDirection direction)
-        {
-            var name = (fk.Alias ?? fk.Name);
-            if (name.StartsWith("FK_"))
-            {
-                name = name.Substring(3);
-            }
-
-            if (name.Contains("!"))
-            {
-                var parts = name.Split('!');
-                return direction == FKDirection.To ? parts[0] : parts[1];
-            }
-
-            return name;
-        }
     }
 
     sealed class CommandAttribute : Attribute
