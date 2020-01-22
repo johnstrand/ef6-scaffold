8c421eb622fdf101c678f67509b9aa1a49f3766e
John Strand <john.strand@dekiru.se>
2020-01-22 12:03:02 +0100

    Added placeholder import command

---
+++ Commands/Import.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScaffoldEF.Commands
+{
+    [Command("import")]
+    static class Import
+    {
+        [Command("fk")]
+        internal static void ForeignKey(string file)
+        {
+            /*
+            DECLARE @v sql_variant 
+            SET @v = N'Test'
+            EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'<SCHEMA>', N'TABLE', N'<TABLE>', N'CONSTRAINT', N'<FK_NAME>'
+            */
+
+        }
+    }
+}
